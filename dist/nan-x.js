3a59260ac86774e72227711ff13b5d36e46087eb
Graham Fairweather <xotic750@gmail.com>
2019-07-19 23:16:12 +0200

    :bookmark: v2.0.16
    Auto build

--- dist/nan-x.js
+++ dist/nan-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-18T16:25:27.863Z",
+  "date": "2019-07-19T21:16:07.544Z",
   "describe": "",
   "description": "The constant NaN derived mathematically by 0 / 0.",
   "file": "nan-x.js",
   "hash": "c49fc031fb73ab0b40c6",
   "license": "MIT",
-  "version": "2.0.15"
+  "version": "2.0.16"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
