e09fc25d3f62132da8efef821c1a24f2337ca129
Graham Fairweather <xotic750@gmail.com>
2019-08-07 02:43:45 +0200

    Auto build

--- dist/nan-x.js
+++ dist/nan-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-08-06T18:39:06.831Z",
+  "date": "2019-08-07T00:43:40.311Z",
   "describe": "",
   "description": "The constant NaN derived mathematically by 0 / 0.",
   "file": "nan-x.js",
