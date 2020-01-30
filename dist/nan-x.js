92c5414c9d9269c6cebcff7f55278cbb1d6464d0
Graham Fairweather <xotic750@gmail.com>
2020-01-30 12:06:18 +0100

    Maintenance

--- dist/nan-x.js
+++ dist/nan-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-09-05T18:20:55.689Z",
+  "date": "2020-01-30T11:06:12.466Z",
   "describe": "",
   "description": "The constant NaN derived mathematically by 0 / 0.",
   "file": "nan-x.js",
